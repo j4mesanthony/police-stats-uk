fb5bb5ee65a7c5b96dd49e8f880bc88d1e7088ea
James Anthony <janthony@greenleaf.com.au>
2023-05-01 17:50:28 +1000

    Fixed toTitleCase logic and updated related tests.

--- src/composables/useStringFormatter.ts
+++ src/composables/useStringFormatter.ts
@@ -5,7 +5,11 @@ interface UseStringFormatter {
 export function useStringFormatter(): UseStringFormatter {
 
     function toTitleCase(input: string): string {
-        return `${input.charAt(0).toUpperCase()}${input.substring(1)}`;
+        const items: Array<string> = input.split(' '),
+              titleCased: Array<string> = items.map(x => `${x.charAt(0).toUpperCase()}${x.substring(1)}`),
+              result: string = titleCased.join(' ');
+
+        return result;
     }
 
     return {
--- tests/useStringFormatter.test.js
+++ tests/useStringFormatter.test.js
@@ -4,14 +4,13 @@ import { useStringFormatter } from '../src/composables/useStringFormatter';
 describe('toTitleCase', () => {
     const { toTitleCase } = useStringFormatter();
 
-    it('should return take in lowercase single word "HOME" and return "Home"', () => {
+    it('should take in lowercase single word "HOME" and return "Home"', () => {
         const result = toTitleCase('home');
         expect(result).toBe('Home');
     });
 
-    // TODO: Fix test
-    it('should return take lowercase string "home area" and return "Home Area"', () => {
-        const result = toTitleCase('home');
+    it('should take lowercase string "home area" and return "Home Area"', () => {
+        const result = toTitleCase('home area');
         expect(result).toBe('Home Area');
     });
 
