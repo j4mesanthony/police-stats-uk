826ecd551a9e37501e9e3c07f8abe8277095fc47
James Anthony <janthony@greenleaf.com.au>
2023-05-02 21:38:04 +1000

    MainNav logic for cached remap to title cased names.

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,20 +1,43 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import { RouteRecord } from 'vue-router';
 import MenuButton from './MenuButton.vue';
 import MenuButtonGroup from './MenuButtonGroup.vue';
 import { useNav } from '../composables/useNav';
 import { useStringFormatter } from '../composables/useStringFormatter';
 
-const { allRoutes, goTo } = useNav();
+const { groupedRoutes, goTo } = useNav();
 const { toTitleCase } = useStringFormatter();
-const routes: Array<RouteRecord> = allRoutes.filter(r => r.meta.isParent);
+
+interface IRoute {
+    name: string,
+    path: string,
+    children?: Array<Route>,
+}
+
+class Route implements IRoute {
+    constructor(public name: string, public path: string, public children: Array<Route>) {};
+}
+
+function recursiveTitleCaseRouteNames(original: Array<RouteRecord>): Array<Route> {
+    let remapped = original.map((x) => {
+        let children = [];
+        if (x.children) children.push(...recursiveTitleCaseRouteNames(x.children as Array<RouteRecord>));
+        return new Route(toTitleCase(x.name as string), x.path, children);
+    });
+
+    return remapped;
+}
+
+const remapped = computed(() => recursiveTitleCaseRouteNames(groupedRoutes));
+
 </script>
-<!-- TODO: Refactor to computed with title casings -->
+
 <template>
     <div class="flex flex-row md:flex-col md:basis-1/5 scroll-smooth overflow-scroll">
-        <MenuButtonGroup  v-for="group in routes" :title="toTitleCase(group.name as string)">
-            <MenuButton v-for="child in group.children" @click="goTo(child.name as string)" small>
-                {{ toTitleCase(child.name as string) }}
+        <MenuButtonGroup  v-for="group in remapped" :title="group.name">
+            <MenuButton v-for="child in group.children" @click="goTo(child.name)" small>
+                {{ child.name }}
             </MenuButton>
         </MenuButtonGroup>
     </div>
--- src/composables/useNav.ts
+++ src/composables/useNav.ts
@@ -8,18 +8,22 @@ interface Route {
 
 interface UseNav {
     allRoutes: Array<RouteRecord>,
+    groupedRoutes: Array<RouteRecord>
     goTo: (path: string) => void,
 }
 
 export function useNav(): UseNav {
     const router: Router = useRouter();
+    const allRoutes: Array<RouteRecord> = router.getRoutes();
+    const groupedRoutes: Array<RouteRecord> = allRoutes.filter(r => r.meta.isParent);
 
     function goTo(path: string): void {
-        router.push({ name: path });
+        router.push({ name: path.toLowerCase() });
     }
     
     return {
-        allRoutes: router.getRoutes(),
+        allRoutes,
+        groupedRoutes,
         goTo,
     }
 }
