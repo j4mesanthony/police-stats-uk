4a5c240cc4d05adc150d7477b40dd4b3aed52bbe
James Anthony <janthony@greenleaf.com.au>
2023-05-15 18:29:55 +1000

    UI work on ForceList.

--- src/views/ForceList.vue
+++ src/views/ForceList.vue
@@ -17,7 +17,20 @@ onMounted(() => {
         
         <div class="flex flex-row flex-wrap">
             <FlexPanelItem>
-                <small>{{ allForces }}</small>
+                <table class="border-collapse table-auto w-full text-sm">
+                    <tr>
+                        <th class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Id</th>
+                        <th class="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Name</th>
+                    </tr>
+                    <tbody class="bg-white dark:bg-slate-800">
+                        <tr class="cursor-pointer hover:bg-slate-50">
+                            <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-left">avon-and-somerset</td>
+                            <td class="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400 text-left">Avon and Somerset Constabulary</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <!-- <small>{{ allForces }}</small> -->
             </FlexPanelItem>
         </div>
     </div>
