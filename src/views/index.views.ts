2da3b401823740696b81ccafa90d7b76b22c3051
James Anthony <janthony@greenleaf.com.au>
2023-05-28 18:26:57 +1000

    Views pulled into index file.

---
+++ src/views/index.views.ts
@@ -0,0 +1,4 @@
+export const Force = () => import('./Force.vue');
+export const ForceList = () => import('./ForceList.vue');
+export const HomeDashboard = () => import('./HomeDashboard.vue');
+export const SettingsArea = () => import('./SettingsArea.vue');
