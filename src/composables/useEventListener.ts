bfd369a2aa7364d7bc17a39423e54d285a5abf55
James Anthony <janthony@greenleaf.com.au>
2023-06-14 10:15:13 +1000

    Removed comment.

--- src/composables/useEventListener.ts
+++ src/composables/useEventListener.ts
@@ -1,7 +1,6 @@
 import { onMounted, onBeforeUnmount } from "vue";
 
 export function useEventListener(type: string, method: any): void {
-    // TODO: Unit test for this composable!
     onMounted(() => {
         window.addEventListener(type, method);
     });
