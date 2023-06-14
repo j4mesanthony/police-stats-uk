7b6dd52df0454f547613f3bce5811361345e67b0
James Anthony <janthony@greenleaf.com.au>
2023-06-14 19:34:50 +1000

    refactor: made ScreenOverlay global component.

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import DataSummaryPanel from './components/DataSummaryPanel.vue';
 import FlexPanelItem from './components/FlexPanelItem.vue';
 import LinkItem from './components/LinkItem.vue';
 import Loader from './components/Loader.vue';
+import ScreenOverlay from './components/ScreenOverlay.vue';
 import StickyHeading from './components/StickyHeading.vue';
 
 const pinia = createPinia();
@@ -20,6 +21,7 @@ app.component('FlexPanelItem', FlexPanelItem);
 app.component('LinkItem', LinkItem);
 app.component('Loader', Loader);
 app.component('RecycleScroller', RecycleScroller);
+app.component('ScreenOverlay', ScreenOverlay);
 app.component('StickyHeading', StickyHeading);
 
 app.use(pinia);
