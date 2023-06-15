61fc7fcd6cac67361a1b45833cdd5bbe98258503
James Anthony <janthony@greenleaf.com.au>
2023-06-15 22:23:15 +1000

    feat: IconButton creation (component to be created)

--- src/components/DataSummaryPanel.vue
+++ src/components/DataSummaryPanel.vue
@@ -38,7 +38,12 @@ function onClick(path: string) {
 </script>
 
 <template>
-    <FlexPanelItem class="flex cursor-pointer basis-80" @click="onClick(props.path)">
+    <FlexPanelItem class="relative flex cursor-pointer basis-80" @click="onClick(props.path)">
+        <!-- TODO: IconButton -->
+        <div class="p-2 absolute top-[7px] right-[7px] rounded-md h-[38px] hover:shadow-md box-border text-center hover:border-cyan-500 border-[1px] border-slate-900 cursor-pointer duration-100 bg-slate-950 w-fit group">
+            <span class="material-symbols-outlined relative text-[20px] transition-transform  text-slate-400 group-hover:text-cyan-500 group-hover:animate-pulse group-hover:scale-[1.3]">expand_content</span>
+        </div>
+
         <p class="clear-both mb-1 text-4xl font-bold">
             <template v-if="hasPath">
                 <LinkItem :underline="false">{{ formattedData }}</LinkItem>
--- src/views/HomeDashboard.vue
+++ src/views/HomeDashboard.vue
@@ -26,11 +26,29 @@ function launchModal() {
   <div>
     <StickyHeading>Dashboard</StickyHeading>
     
-    <div class="flex flex-row flex-wrap gap-5">
+    <div class="flex flex-row flex-wrap gap-5 mb-10">
       <DataSummaryPanel label="Active Forces" :data="forcesCount" :path="ACTIVE_FORCES" />
       <DataSummaryPanel label="Crimes" :data="0" @click="launchModal" />
       <DataSummaryPanel label="Investigations" :data="0" />
     </div>
+
+    <StickyHeading>Buttons</StickyHeading>
+    <div class="flex flex-row flex-wrap gap-3 mb-10">
+      <!-- TODO: IconButton -->
+      <div class="p-2 top-[7px] right-[7px] rounded-md h-[38px] hover:shadow-md box-border text-center hover:border-cyan-500 border-[1px] border-slate-900 cursor-pointer duration-100 bg-slate-950 w-fit group">
+        <span class="material-symbols-outlined relative text-[20px] transition-transform  text-slate-400 group-hover:text-cyan-500 group-hover:animate-pulse group-hover:scale-[1.3]">close</span>
+      </div>
+      <div class="p-2 top-[7px] right-[7px] rounded-md h-[38px] hover:shadow-md box-border text-center hover:border-cyan-500 border-[1px] border-slate-900 cursor-pointer duration-100 bg-slate-950 w-fit group">
+        <span class="material-symbols-outlined relative text-[20px] transition-transform  text-slate-400 group-hover:text-cyan-500 group-hover:animate-pulse group-hover:scale-[1.3]">done</span>
+      </div>
+      <div class="p-2 top-[7px] right-[7px] rounded-md h-[38px] hover:shadow-md box-border text-center hover:border-cyan-500 border-[1px] border-slate-900 cursor-pointer duration-100 bg-slate-950 w-fit group">
+        <span class="material-symbols-outlined relative text-[20px] transition-transform  text-slate-400 group-hover:text-cyan-500 group-hover:animate-pulse group-hover:scale-[1.3]">search</span>
+      </div>
+      <div class="p-2 top-[7px] right-[7px] rounded-md h-[38px] hover:shadow-md box-border text-center hover:border-cyan-500 border-[1px] border-slate-900 cursor-pointer duration-100 bg-slate-950 w-fit group">
+        <span class="material-symbols-outlined relative text-[20px] transition-transform  text-slate-400 group-hover:text-cyan-500 group-hover:animate-pulse group-hover:scale-[1.3]">expand_content</span>
+      </div>
+    </div>
+
 
     <Teleport to="body">
       <Transition name="fade">
