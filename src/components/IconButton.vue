fe5d174aa2752720e80c60c48233248a652eab3f
James Anthony <janthony@greenleaf.com.au>
2023-06-16 20:39:59 +1000

    style: made IconButton unselectable

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -14,7 +14,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div :class="{ 'hover:shadow-md bg-slate-950 hover:border-cyan-500 border-[1px] border-slate-900': props.showOutline }" class="p-2 top-[7px] right-[7px] rounded-md h-[38px] box-border text-center  cursor-pointer duration-100 w-fit group">
+    <div :class="{ 'hover:shadow-md bg-slate-950 hover:border-cyan-500 border-[1px] border-slate-900': props.showOutline }" class="p-2 top-[7px] right-[7px] rounded-md h-[38px] box-border text-center select-none cursor-pointer duration-100 w-fit group">
         <span class="material-symbols-outlined relative text-[20px] transition-transform  text-slate-400 group-hover:text-cyan-500 group-hover:animate-pulse group-hover:scale-[1.2]">{{ props.icon }}</span>
     </div>
 </template>
